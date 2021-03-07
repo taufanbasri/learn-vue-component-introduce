79a40464924c4ab452c80f270afa3935404087da
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2021-03-07 14:51:50 +0700

    Introducing Component

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
     data() {
         return {
-            detailsAreVisible: false,
             friends: [
                 {
                     id: 1,
@@ -16,6 +15,30 @@ const app = Vue.createApp({
                     email: 'julie@localhost.com'
                 }
             ]
+        }
+    },
+});
+
+app.component('friend-contact', {
+    template: `
+        <li>
+            <h2>{{ friend.name }}</h2>
+            <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+            <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ friend.phone }}</li>
+            <li><strong>Email:</strong> {{ friend.email }}</li>
+            </ul>
+        </li>
+    `,
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend: {
+                id: 2,
+                name: 'Julie Jones',
+                phone: '09876 543 221',
+                email: 'julie@localhost.com'
+            }
         }
     },
     methods: {
